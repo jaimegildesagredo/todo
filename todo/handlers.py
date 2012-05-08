10b552dcec0448c902a20929a52eaecdd4a8640c
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2012-05-08 16:47:30 +0200

    Tasks are not done by default

--- todo/handlers.py
+++ todo/handlers.py
@@ -15,9 +15,7 @@ class IndexHandler(web.RequestHandler):
         self.render('index.html', tasks=self.store.find(models.Task))
 
     def post(self):
-        self.store.add(models.Task(
-            body=self.get_argument('body'),
-            done=False))
+        self.store.add(models.Task(body=self.get_argument('body')))
 
         self.store.commit()
 
